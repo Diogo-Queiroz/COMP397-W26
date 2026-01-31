b93cd33d34722e8563193a5fe36b96016a137d75
Diogo Queiroz <diogoqueirooz1212@gmail.com>
2026-01-30 19:04:15 -0500

    Fix for Lab2 Camera Rotation and Lights and Shadows.

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -12,6 +12,7 @@ public class PlayerInput : MonoBehaviour
     private Vector3 velocity;
     [SerializeField] private float rotationSpeed = 4.0f;
     [SerializeField] private float mouseSensY = 5.0f;
+    private float camXRotation;
     [SerializeField, Self] private CharacterController controller;
     [SerializeField, Child] private Camera cam;
     private void OnValidate()
@@ -23,12 +24,14 @@ public class PlayerInput : MonoBehaviour
     {
         move = InputSystem.actions.FindAction("Player/Move");
         look = InputSystem.actions.FindAction("Player/Look");
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     void Update()
     {
         Vector2 readMove = move.ReadValue<Vector2>();
-        Vector2 readLook = look.ReadValue<Vector2>();
+        Vector2 readLook = look.ReadValue<Vector2>();// (0,0)
         // Movement of the player
         Vector3 movement = transform.right * readMove.x + transform.forward * readMove.y;
         velocity.y += gravity * Time.deltaTime;
@@ -40,8 +43,8 @@ public class PlayerInput : MonoBehaviour
         transform.Rotate(Vector3.up, readLook.x * rotationSpeed * Time.deltaTime);
 
         // Rotate the Camera
-        mouseSensY = mouseSensY * readLook.y;
-        mouseSensY = Mathf.Clamp(mouseSensY, -90f, 90f);
-        cam.gameObject.transform.localRotation = Quaternion.Euler(mouseSensY, 0, 0);
+        camXRotation += mouseSensY * readLook.y * Time.deltaTime * -1;
+        camXRotation = Mathf.Clamp(camXRotation, -90f, 90f);
+        cam.gameObject.transform.localRotation = Quaternion.Euler(camXRotation, 0, 0);
     }
 }
