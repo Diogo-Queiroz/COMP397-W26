6f0552b9b1aaeeabc8457f354083424ded5e16ec
DiogoQueiroz <diogoqueirooz1212@gmail.com>
2026-04-10 19:22:29 -0400

    newGameDataEvent on EventChannel

--- Assets/Scripts/ObserverPattern/EventChannelManager.cs
+++ Assets/Scripts/ObserverPattern/EventChannelManager.cs
@@ -8,5 +8,6 @@ public class EventChannelManager : PersistentSingleton<EventChannelManager>
 {
     public VoidEventChannel voidEvent;
     public FloatEventChannel floatEvent;
+    public GameDataEventChannel newGameDataEventChannel;
     public GameDataEventChannel gameDataEvent;
 }
